2ecebbcf63041353f51613a2b5ab3c2283c36e89
LtMilligan <gioele.miscia@gmail.com>
2024-07-18 16:27:45 +0200

    [UPD] Array created

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            coseDaFare: [
+                {
+                    text: 'Tagliare il prato',
+                    done: false
+                },
+                {
+                    text: 'Comprare il cibo per Wolfango',
+                    done: false
+                },
+                {
+                    text: 'Giocare a Tekken 8',
+                    done: false
+                },
+                {
+                    text: 'Buttare la spazzatura',
+                    done: false
+                },
+                {
+                    text: 'Chiamare Rivaldo per quel lavoretto con il rame',
+                    done: false
+                },
+                {
+                    text: 'Controllare il prezzo di BitCoin',
+                    done: false
+                }
+
+            ]
+        }
+    }
+}).mount('#app')
